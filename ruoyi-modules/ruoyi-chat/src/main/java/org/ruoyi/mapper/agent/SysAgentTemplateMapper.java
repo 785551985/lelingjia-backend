fff811d6b712e05ace7872df4a69febb462f85a9
Enterprise Developer <admin@enterprise.local>
2026-07-31 14:00:10 +0800

    fix: add InterceptorIgnore and tenant_id column for platform template tables

--- ruoyi-modules/ruoyi-chat/src/main/java/org/ruoyi/mapper/agent/SysAgentTemplateMapper.java
+++ ruoyi-modules/ruoyi-chat/src/main/java/org/ruoyi/mapper/agent/SysAgentTemplateMapper.java
@@ -1,13 +1,16 @@
 package org.ruoyi.mapper.agent;
 
+import com.baomidou.mybatisplus.annotation.InterceptorIgnore;
 import org.ruoyi.common.mybatis.core.mapper.BaseMapperPlus;
 import org.ruoyi.domain.entity.agent.SysAgentTemplate;
 
 /**
  * 平台统一智能体模板 Mapper 接口
+ * (平台公共打底模版资产，使用 InterceptorIgnore 忽略多租户租户隔离)
  *
  * @author ruoyi
  */
+@InterceptorIgnore(tenantLine = "true")
 public interface SysAgentTemplateMapper extends BaseMapperPlus<SysAgentTemplate, SysAgentTemplate> {
 
 }
--- ruoyi-modules/ruoyi-chat/src/main/java/org/ruoyi/mapper/knowledge/SysKnowledgeTemplateMapper.java
+++ ruoyi-modules/ruoyi-chat/src/main/java/org/ruoyi/mapper/knowledge/SysKnowledgeTemplateMapper.java
@@ -1,13 +1,16 @@
 package org.ruoyi.mapper.knowledge;
 
+import com.baomidou.mybatisplus.annotation.InterceptorIgnore;
 import org.ruoyi.common.mybatis.core.mapper.BaseMapperPlus;
 import org.ruoyi.domain.entity.knowledge.SysKnowledgeTemplate;
 
 /**
  * 平台统一知识库范本 Mapper 接口
+ * (平台公共打底模版资产，使用 InterceptorIgnore 忽略多租户租户隔离)
  *
  * @author ruoyi
  */
+@InterceptorIgnore(tenantLine = "true")
 public interface SysKnowledgeTemplateMapper extends BaseMapperPlus<SysKnowledgeTemplate, SysKnowledgeTemplate> {
 
 }
